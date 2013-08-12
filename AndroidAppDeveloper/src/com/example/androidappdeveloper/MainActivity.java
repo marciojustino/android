a4834d9d028fbacf3390794aef46bbcf2bf98b60
Marcio Justino <marciojustino@gmail.com>
2013-08-12 16:47:47 -0300

    studding fragments in activity

--- AndroidAppDeveloper/src/com/example/androidappdeveloper/MainActivity.java
+++ AndroidAppDeveloper/src/com/example/androidappdeveloper/MainActivity.java
@@ -21,30 +21,30 @@ public class MainActivity extends Activity {
 
 	@Override
 	public void onPause() {
-		super.onPause();	// Always call the superclass method first
-		
-		// 1. Persist any user's change that needs recovery... 
-		
+		super.onPause(); // Always call the superclass method first
+
+		// 1. Persist any user's change that needs recovery...
+
 		// 2. Release active resources...
 	}
-	
+
 	@Override
-	public void onResume(){
-		super.onResume();	// Always call the superclass method first
-		
+	public void onResume() {
+		super.onResume(); // Always call the superclass method first
+
 		// 1. Initialize resources that released in onPause event...
 	}
 
 	@Override
 	public void onStop() {
 		super.onStop();
-		
+
 		// 1. Finalize all active resources...
 	}
-	
+
 	@Override
-	public void onRestart(){
-		super.onRestart();	// Always call the superclass method first
+	public void onRestart() {
+		super.onRestart(); // Always call the superclass method first
 
 		// 1. Recover saved data in onStop event...
 	}
@@ -59,13 +59,21 @@ public class MainActivity extends Activity {
 	public void sendMessage(View view) {
 		// 1. Create new activity...
 		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		
+
 		// 2. Get data to send to new activity...
 		EditText edit = (EditText) findViewById(R.id.edit_message);
 		String message = edit.getText().toString();
 
 		// 1. Send data to DisplayMessageActivity activity...
 		intent.putExtra(EXTRA_MESSAGE, message);
+
+		// 2. Start new activity...
+		startActivity(intent);
+	}
+
+	public void onFragmentClick(View view) {
+		// 1. Create new activity...
+		Intent intent = new Intent(this, DisplayFragmentActivity.class);
 		
 		// 2. Start new activity...
 		startActivity(intent);
