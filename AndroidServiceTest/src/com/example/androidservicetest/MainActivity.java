4f6e8200556ca15fb54f5c45ae858ca6753875a6
Marcio Justino <marciojustino@gmail.com>
2013-10-03 09:56:31 -0300

    Finalização da 1ª fase do projeto teste de serviços de download. Ainda será testado mas a codificação da Activity e do serviço já estão ok.

--- AndroidServiceTest/src/com/example/androidservicetest/MainActivity.java
+++ AndroidServiceTest/src/com/example/androidservicetest/MainActivity.java
@@ -2,14 +2,59 @@ package com.example.androidservicetest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
+
+	private TextView textView;
+
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(android.content.Context context, Intent intent) {
+			Bundle bundle = intent.getExtras();
+			if (bundle != null) {
+				String path = bundle.getString(DownloadService.FILEPATH);
+				int resultCode = bundle.getInt(DownloadService.RESULT);
+
+				if (resultCode == RESULT_OK) {
+					Toast.makeText(MainActivity.this,
+							"Download complete. Download URI: " + path,
+							Toast.LENGTH_LONG).show();
+					textView.setText("Download is done!");
+				} else {
+					Toast.makeText(MainActivity.this, "Download failed",
+							Toast.LENGTH_LONG).show();
+					textView.setText("Download failed");
+				}
+			}
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// Get status object of layout
+		textView = (TextView) findViewById(R.id.status);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		registerReceiver(receiver, new IntentFilter(DownloadService.NOTIFICATION));
+	}
+	
+	@Override
+	protected void onPause() {		
+		super.onPause();
+		unregisterReceiver(receiver);
 	}
 
 	@Override
@@ -17,6 +62,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}	
+	
+	public void onClick(View view){
+		// Start download server...
+		Intent intent = new Intent(this, DownloadService.class);
+		intent.putExtra(DownloadService.FILEPATH, "index.html");
+		intent.putExtra(DownloadService.URL, "http://www.vogella.com/index.html");
+		startService(intent);
+		textView.setText("Service started");
 	}
-
 }
