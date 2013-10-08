2df6c39aeeb58d7d1e2dbc0c1f49f655fdde4382
marciojustino <marciojustino@gmail.com>
2013-10-07 21:48:57 -0300

    ajustes no serviço para permitir maior controle ao tentar utilizar o storage externo.

--- AndroidServiceTest/src/com/example/adroitest/service/DownloadService.java
+++ AndroidServiceTest/src/com/example/adroitest/service/DownloadService.java
@@ -1,85 +1,91 @@
 package com.example.adroitest.service;
 
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Environment;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import android.app.Activity;
-import android.app.IntentService;
-import android.content.Intent;
-import android.os.Environment;
 import java.net.URL;
 
 public class DownloadService extends IntentService {
-	private int result = Activity.RESULT_CANCELED;
-	public static final String URL = "urlpath";
-	public static final String FILENAME = "filename";
-	public static final String FILEPATH = "filepath";
-	public static final String RESULT = "result";
-	public static final String NOTIFICATION = "com.example.androidservicetest";
+    public static final String URL = "urlpath";
+    public static final String FILENAME = "filename";
+    public static final String FILEPATH = "filepath";
+    public static final String RESULT = "result";
+    public static final String NOTIFICATION = "com.example.adroitest.service";
+    private int result = Activity.RESULT_CANCELED;
 
-	public DownloadService(String name) {
-		super(name);
-	}
+    public DownloadService() {
+        super("DownloadService");
+    }
 
-	// Will be called asynchronously be Android...
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		String urlPath = intent.getStringExtra(URL);
-		String fileName = intent.getStringExtra(FILENAME);
+    // Will be called asynchronously be Android...
+    @Override
+    protected void onHandleIntent(Intent intent) {
 
-		File output = new File(Environment.getExternalStorageDirectory(),
-				fileName);
+        if (Environment.getExternalStorageState().equals("MEDIA_MOUNTED")) {
+            String urlPath = intent.getStringExtra(URL);
+            String fileName = intent.getStringExtra(FILENAME);
 
-		if (output.exists()) {
-			output.delete();
-		}
+            File output = new File(Environment.getExternalStorageDirectory(),
+                    fileName);
 
-		InputStream stream = null;
-		FileOutputStream fos = null;
+            if (output.exists()) {
+                output.delete();
+            }
 
-		try {
-			URL url = new URL(urlPath);
-			stream = url.openConnection().getInputStream();
-			InputStreamReader reader = new InputStreamReader(stream);
-			fos = new FileOutputStream(output.getPath());
-			int next = -1;
-			while ((next = reader.read()) != -1) {
-				fos.write(next);
-			}
+            InputStream stream = null;
+            FileOutputStream fos = null;
 
-			// Download successful finished...
-			result = Activity.RESULT_OK;
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// Free memory objects...
-			if (stream != null) {
-				try {
-					stream.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+            try {
+                URL url = new URL(urlPath);
+                stream = url.openConnection().getInputStream();
+                InputStreamReader reader = new InputStreamReader(stream);
+                fos = new FileOutputStream(output.getPath());
+                int next;
+                while ((next = reader.read()) != -1) {
+                    fos.write(next);
+                }
 
-			if (fos != null) {
-				try {
-					fos.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+                // Download successful finished...
+                result = Activity.RESULT_OK;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                // Free memory objects...
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
 
-		publishResults(output.getAbsolutePath(), result);
-	}
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-	private void publishResults(String outputPath, int result) {
-		Intent intent = new Intent(NOTIFICATION);
-		intent.putExtra(FILEPATH, outputPath);
-		intent.putExtra(RESULT, result);
-		// Send broadcast do system...
-		sendBroadcast(intent);
-	}
+            publishResults(output.getAbsolutePath(), result);
+        } else {
+            publishResults("External storage doesn't has mounted", Activity.RESULT_CANCELED);
+        }
+    }
+
+    private void publishResults(String outputPath, int result) {
+        Intent intent = new Intent(NOTIFICATION);
+        intent.putExtra(FILEPATH, outputPath);
+        intent.putExtra(RESULT, result);
+        // Send broadcast do system...
+        sendBroadcast(intent);
+    }
 }
